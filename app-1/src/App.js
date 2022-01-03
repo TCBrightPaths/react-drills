10b279509bd43a516d63ba9c02a4095707f1f1e3
Tamara Chance <tamara.n.chance@gmail.com>
2022-01-03 14:30:48 -0600

    magic text

--- app-1/src/App.js
+++ app-1/src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [input, setInput] = useState("")
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input 
+        type="text"  
+        value={ input }
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <div className="content">
+        <h2>{ input }</h2>
+      </div>
     </div>
   );
 }
